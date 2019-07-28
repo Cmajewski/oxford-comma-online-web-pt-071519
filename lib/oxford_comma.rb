47346a8f10e25c96f8d6ec65f822bf755282d528
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-07-28 23:43:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.length==2
+  array[0]<< " and"
+  array.join (" ")
+  elsif array.length==1
+ array.join
+ else
+ array[0..-2].join(", ")<<", and "<<array[-1]
+  end
 end
