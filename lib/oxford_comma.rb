34535a214aaa0084576fd5476e58a7cc077de971
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-07-28 23:19:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
-  if array.length==2
-  array[0]<< " and"
-  array.join (" ")
-  elsif array.length==1
-   array.join
- else array.length>2
-   array.to_sentence
-  end
+  array.to_sentence
 end
+  #if array.length==2
+  #array[0]<< " and"
+  #array.join (" ")
+  #elsif array.length==1
+   #array.join
+ #else array.length>2
+  # array.to_sentence
+  #end
+#end
